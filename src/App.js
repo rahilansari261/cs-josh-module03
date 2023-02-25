e77ac96bc2629835773881483b520a419e088628
Rahil Ansari <12849916+rahilansari261@users.noreply.github.com>
2023-02-25 08:08:25 +0000

    ex 2 solved

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css'
-import { Quote } from './Quote'
+// import { Quote } from './Quote'
+// import { FrequentlyAskedQuestion } from './Faq'
+import { LoginForm } from './Login'
 
 function App() {
   return (
-    <Quote
-      by='Bill Gates (Allegedly)'
-      source='https://quoteinvestigator.com/2011/09/08/640k-enough/'
-    >
-      640kb of memory ought to be enough for anyone
-    </Quote>
+    // <Quote
+    //   by='Bill Gates (Allegedly)'
+    //   source='https://quoteinvestigator.com/2011/09/08/640k-enough/'
+    // >
+    //   640kb of memory ought to be enough for anyone
+    // </Quote>
+
+    // <FrequentlyAskedQuestion
+    //   question='What does “CSS” stand for?'
+    //   answer='Cool Styling Strategy'
+    // />
+
+    <LoginForm
+      handleSubmit={(ev) => {
+        ev.preventDefault()
+        alert('Submitted!')
+      }}
+    />
   )
 }
 
---
+++ src/Faq.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import styled from 'styled-components'
+
+export function FrequentlyAskedQuestion({ question, answer }) {
+  return (
+    <Wrapper className='faq'>
+      <Question>{question}</Question>
+      <Answer className='answer'>{answer}</Answer>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.details`
+  font-size: 1rem;
+  color: hsl(0deg 0% 25%);
+  margin: 32px;
+  border: 2px solid;
+  border-radius: 4px;
+`
+const Question = styled.summary`
+  cursor: pointer;
+  padding: 12px 16px;
+  outline-offset: 5px;
+`
+const Answer = styled.div`
+  background: hsl(0deg 0% 92.5%);
+  border-radius: 0 0 4px 4px;
+  padding: 12px 17px;
+  font-style: italic;
+`
