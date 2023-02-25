610f1664814d76508040622b5328629e6fa66475
Rahil Ansari <12849916+rahilansari261@users.noreply.github.com>
2023-02-25 07:24:06 +0000

    prettier added

--- src/Quote.js
+++ src/Quote.js
@@ -1,37 +1,30 @@
-import React from 'react';
+import React from 'react'
 
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const Quote = ({ by, source, children }) => {
   return (
-
     <Wrapper>
-      <QuoteContent>
-        {children}
-      </QuoteContent>
+      <QuoteContent>{children}</QuoteContent>
       <figcaption>
         <Author>
-          <SourceLink href={source}>
-            {by}
-          </SourceLink>
+          <SourceLink href={source}>{by}</SourceLink>
         </Author>
       </figcaption>
     </Wrapper>
-
-  );
-};
+  )
+}
 
 const Wrapper = styled.div`
-position:absolute;
-top: 0px;
+  position: absolute;
+  top: 0px;
   left: 0px;
   right: 0px;
   bottom: 0px;
-margin:auto;
-max-width:350px;
-height: 100px;
-
-`;
+  margin: auto;
+  max-width: 350px;
+  height: 100px;
+`
 const QuoteContent = styled.div`
   margin: 0;
   background: hsl(0deg 0% 90%);
@@ -46,14 +39,14 @@ const QuoteContent = styled.div`
   &::after {
     content: '”';
   }
-`;
+`
 
 const Author = styled.div`
- font-style: italic;
+  font-style: italic;
   display: block;
   text-align: right;
   margin-top: 8px;
-`;
+`
 
 const SourceLink = styled.a`
   text-decoration: none;
@@ -62,5 +55,4 @@ const SourceLink = styled.a`
   &::before {
     content: '—';
   }
-`;
-
+`
