e733b551e4208b9315a01e722f087e753d532ea2
Rahil Ansari <12849916+rahilansari261@users.noreply.github.com>
2023-02-27 08:12:11 +0000

    wallart changes

--- src/WallArt.js
+++ src/WallArt.js
@@ -10,8 +10,8 @@ export function WallArt({ src, alt, caption, width }) {
         src={src}
         alt={alt}
         style={{
-          width,
-          height,
+          '--width': width + 'px',
+          '--height': height + 'px',
         }}
       />
       <Caption>{caption}</Caption>
@@ -30,16 +30,20 @@ const Wrapper = styled.div`
     0 21.4px 24.5px -2px rgba(0, 0, 0, 0.073),
     0 -15px 40px -20px rgba(0, 0, 0, 0.2);
   border-radius: 8px;
+  width: 250px;
+  margin : 150px auto;
 `
 const Photo = styled.img`
   display: block;
   border-radius: 4px;
+  width: var(--width);
+  height: var(--height);
 `
 const Caption = styled.p`
   position: absolute;
   left: 8px;
   right: 8px;
-  bottom: 8px;
+  bottom: -8px;
   text-align: center;
   padding: 8px;
   background: hsl(0deg 0% 100% / 0.9);
